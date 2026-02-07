d590183b41a84ad34e30e5e24d87c827eb72f894
MD RAHATUL ISLAM <rahathridoyd2x@gmail.com>
2026-02-07 07:28:23 +0600

    1.0.3 F: fix route.ts

--- frontend/app/(private)/dashboard/page.tsx
+++ frontend/app/(private)/dashboard/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 
-const Dashboard = () => {
+import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
   return (
     <main>
       <h1 className='text-3xl mb-3'>Welcome to Dashboard</h1>
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -9,6 +9,7 @@ export const config = {
   matcher: [
 
     "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/dashboard",
     
   ],
 };
